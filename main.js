cf9ce53c4c89695bc0fdf87acb9f7971d3648b7c
sheerin-farhana <sheerinfarhana25@gmail.com>
2023-06-30 12:33:15 +0530

    Edit feature done

--- main.js
+++ main.js
@@ -8,27 +8,51 @@ let itemList = document.getElementById('items');
                         }
             //let userdetails_obj = JSON.stringify(details_obj);
             //localStorage.setItem(event.target.name.value ,userdetails_obj );
-            axios.post("https://crudcrud.com/api/d3833744c4ba4b649324e059604704ac/bookingData",details_obj)
+
+            axios.post("https://crudcrud.com/api/53f18e8b61c94682aeb50395ac8e26f7/bookingData",details_obj)
             .then((response) => {
                 console.log(response);
-                
+                let li = document.createElement('li');
+                    let deleteButton = document.createElement('input');
+                    deleteButton.type = "button"
+                    deleteButton.id = "delete";
+                    deleteButton.value = "DELETE";
+                    //console.log(deleteButton);
+                    let editButton = document.createElement('input');
+                    editButton.type = "button"
+                    editButton.id = "edit";
+                    editButton.value = "Edit";
+                    li.appendChild(document.createTextNode(response.data.name + " - "));
+                    li.appendChild(document.createTextNode(response.data.email + " - "));
+                    li.appendChild(document.createTextNode(response.data.phone ));
+                    li.appendChild(deleteButton);
+                    li.appendChild(editButton);
+                    itemList.appendChild(li)
             })
             .catch((err) => {
                 //console.log(err);
                 document.body.innerHTML = document.body.innerHTML + "<h4>Something WENt Wrong</h4>";
-            })  
+            }) 
+
+            document.getElementById('name').value = "";
+            document.getElementById('email').value = "";
+            document.getElementById('phone').value = "";
+            
+            
         }
         
+
+        
+
         window.addEventListener("DOMContentLoaded",()=>{
-            axios.get("https://crudcrud.com/api/d3833744c4ba4b649324e059604704ac/bookingData")
+            axios.get("https://crudcrud.com/api/53f18e8b61c94682aeb50395ac8e26f7/bookingData")
             .then(response => {
-                //console.log(response.data);
+                console.log(response.data);
 
                 for(let i=0; i<response.data.length;i++){
 
+
                     let li = document.createElement('li');
-                    //li.className = "list-group-item"
-                    //li.className = "bg-light"
                     let deleteButton = document.createElement('input');
                     deleteButton.type = "button"
                     deleteButton.id = "delete";
@@ -39,6 +63,17 @@ let itemList = document.getElementById('items');
                     editButton.id = "edit";
                     editButton.value = "Edit";
                     deleteButton.addEventListener('click',ondelete);
+                    li.appendChild(document.createTextNode(response.data[i].name + " - "));
+                    li.appendChild(document.createTextNode(response.data[i].email + " - "));
+                    li.appendChild(document.createTextNode(response.data[i].phone ));
+                    li.appendChild(deleteButton);
+                    li.appendChild(editButton);
+                    itemList.appendChild(li)
+
+                   
+                    //li.className = "list-group-item"
+                    //li.className = "bg-light"
+                    
                     let keyValue = response.data.name;
                     function ondelete(e)
                     {
@@ -46,7 +81,7 @@ let itemList = document.getElementById('items');
                         let parentNode = e.target.parentNode;
                         parentNode.remove();
                         //let details = e.target
-                        let dataID = `https://crudcrud.com/api/d3833744c4ba4b649324e059604704ac/bookingData/${response.data[i]._id}`;
+                        let dataID = `https://crudcrud.com/api/53f18e8b61c94682aeb50395ac8e26f7/bookingData/${response.data[i]._id}`;
             
                         axios.delete(dataID)
                         .then((r) =>{
@@ -55,29 +90,36 @@ let itemList = document.getElementById('items');
                         .catch(error => console.log(error));
                     }
                     editButton.addEventListener('click',onEdit);
-                    let name = response.data.name
-                    let email = response.data.email;
-                    let phone = response.data.phone;
+                    let name = response.data[i].name
+                    let email = response.data[i].email;
+                    let phone = response.data[i].phone;
         function onEdit(e){
             e.preventDefault();
             let parentNode = e.target.parentNode;
             parentNode.remove();
             //let details = e.target
-            localStorage.removeItem(keyValue);
+            //localStorage.removeItem(keyValue);
             document.getElementById('name').value = name;
             document.getElementById('email').value = email;
             document.getElementById('phone').value = phone;
-        }
 
 
+            let dataID = `https://crudcrud.com/api/53f18e8b61c94682aeb50395ac8e26f7/bookingData/${response.data[i]._id}`;
+            axios.delete(dataID)
+            .then(response => {
+                console.log(response);
+                alert('DATA UPDATED')
+            })
 
-                    li.appendChild(document.createTextNode(response.data[i].name + " - "));
+
+                   /* li.appendChild(document.createTextNode(response.data[i].name + " - "));
                     li.appendChild(document.createTextNode(response.data[i].email + " - "));
                     li.appendChild(document.createTextNode(response.data[i].phone ));
                     li.appendChild(deleteButton);
                     li.appendChild(editButton);
-                    itemList.appendChild(li)
+                    itemList.appendChild(li)*/
                 }
+            }
             })
             .catch(error => console.log(error));
         })
